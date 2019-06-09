8e993ed9100d3f47bcf7472dfbed8e57c174b1e0
sonNN <son.nn@altplus.com.vn>
2019-06-09 22:34:37 +0700

    add ws

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Router, Route, Switch} from 'react-router-dom'
+import {history} from './history'
+import Privacy from './bitmex/component'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router history={history}>
+        <Switch>
+          <Route path='/privacy' component={Privacy}/>
+        </Switch>
+    </Router>
+  )
 }
-
-export default App;
+export default App
